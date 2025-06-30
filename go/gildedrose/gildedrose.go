c6a8bf6d10545ef7698ad8e0c0c89259bf68680d
saimanojk1 <saimanojk1@hotmail.com>
2025-06-30 18:19:51 +0530

    Refactor some to switch

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -7,34 +7,33 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for _, item := range items {
-
-		if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if item.Quality > 0 {
-				if item.Name != "Sulfuras, Hand of Ragnaros" {
-					item.Quality = item.Quality - 1
-				}
-			}
-		} else {
+		switch item.Name {
+		case "Aged Brie":
 			if item.Quality < 50 {
-				item.Quality = item.Quality + 1
-				if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+				item.Quality++
+			}
+			item.SellIn--
+		case "Backstage passes to a TAFKAL80ETC concert":
+			if item.Quality < 50 {
+				item.Quality++
+				if item.Quality < 50 {
 					if item.SellIn < 11 {
-						if item.Quality < 50 {
-							item.Quality = item.Quality + 1
-						}
+						item.Quality++
 					}
 					if item.SellIn < 6 {
-						if item.Quality < 50 {
-							item.Quality = item.Quality + 1
-						}
+						item.Quality++
 					}
 				}
 			}
+			item.SellIn--
+		case "Sulfuras, Hand of Ragnaros":
+		default:
+			if item.Quality > 0 {
+				item.Quality--
+			}
+			item.SellIn--
 		}
 
-		if item.Name != "Sulfuras, Hand of Ragnaros" {
-			item.SellIn = item.SellIn - 1
-		}
 
 		if item.SellIn < 0 {
 			if item.Name != "Aged Brie" {
