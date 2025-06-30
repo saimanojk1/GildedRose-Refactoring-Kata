9855f9c14619a510c8f458757404030a625a170e
saimanojk1 <saimanojk1@hotmail.com>
2025-06-30 17:18:42 +0530

    Add proper tests

--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -8,12 +8,34 @@ import (
 
 func Test_Foo(t *testing.T) {
 	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+		{"+5 Dexterity Vest", 10, 20},
+		{"Aged Brie", 2, 0},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+		{"Conjured Mana Cake", 3, 6},
 	}
 
+	var expectedItems = []*gildedrose.Item{
+		{"+5 Dexterity Vest", 9, 19},
+		{"Aged Brie", 1, 1},
+		{"Elixir of the Mongoose", 4, 6},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 14, 21},
+		{"Backstage passes to a TAFKAL80ETC concert", 9, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", 4, 50},
+		{"Conjured Mana Cake", 2, 5},
+	}
 	gildedrose.UpdateQuality(items)
 
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	for i, _ := range items {
+		if items[i].Name != expectedItems[i].Name || items[i].Quality != expectedItems[i].Quality || items[i].SellIn != expectedItems[i].SellIn {
+			t.Errorf("Name: Expected %v but got %v ", expectedItems[i], items[i])
+		}
 	}
+
 }
